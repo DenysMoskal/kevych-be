e64df17ab3aa000d6059d1a9f9257a284f685936
Denys <denysmosol@gmail.com>
2025-04-12 21:57:41 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://kevych-fe.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://kevych-fe.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
